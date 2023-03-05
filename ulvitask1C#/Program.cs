b368cbc34a8fb5e9a098641ec9e7f0555066b651
ulvee <ulvee@mail.ru>
2023-03-05 18:48:43 +0400

    first modified ulvi

---
+++ ulvitask1C#/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int ilkqiymet = 400; 
+
+if (ilkqiymet < 100)
+{
+    Console.WriteLine("a = 380");
+}
+else if (ilkqiymet > 1000) 
+{
+    Console.WriteLine("a = 360");
+}
+else
+{
+    Console.WriteLine("a = 368");
+}
